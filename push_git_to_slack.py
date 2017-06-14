fdbcaab348eb86a71955f6422ab0fa8ee2482167
EC2 Default User <ec2-user@ip-172-31-36-250.eu-west-1.compute.internal>
2017-06-14 19:44:40 +0000

    Added Push version

--- push_git_to_slack.py
+++ push_git_to_slack.py
@@ -3,38 +3,6 @@ import requests, json
 from datetime import datetime
 from dateutil.parser import parse
 
-def repos_list(git_api_url, org_name, api_user, token):
-    
-    """
-    Lists all repositories of the given org
-    Returns the list of repository names
-    """
-
-    url = git_api_url + "/orgs/" + org_name + "/repos"
-
-    #print("List URL: " + url)
-    #print("User: " + api_user)
-    #print("Token: " + token)
-
-    #s = requests.Session()
-    #s.headers.update({'access_token': token})
-
-    tokenHeader = {'Authorization': 'token %s' % token}
-    req = requests.get(url, headers=tokenHeader)  
-    
-    if req.status_code == 200:
-
-        data = req.json()
-        repoList = []
-        for currentRepo in data:
-            repoName = currentRepo["name"]
-            repoList.append(repoName)
-
-        return repoList
-    else:
-        print("Repo List Error Code: {} ".format(req.status_code))
-        raise SystemExit
-
 
 def pulls_list(git_api_url, org_name, repo_name, api_user, token):
     
@@ -42,7 +10,7 @@ def pulls_list(git_api_url, org_name, repo_name, api_user, token):
     Lists all open pull requests on a given repository 
     Returns a list of tuples containing some information for each pull request
     """
-    url = git_api_url + "/repos/" + org_name + "/" + repo_name + "/pulls?status=open" 
+    url = git_api_url + "/repos/" + org_name + "/" + repo_name + "/pulls?state=all" 
     #print("List URL: " + url)
     tokenHeader = {'Authorization': 'token %s' % token}    
     req = requests.get(url, headers=tokenHeader)  
@@ -60,8 +28,9 @@ def pulls_list(git_api_url, org_name, repo_name, api_user, token):
             #creation_date = datetime.strptime(currentPull["created_at"], '%Y-%m-%dT%H:%M:%SZ')
             creation_date = parse(currentPull["created_at"])
             html_url= currentPull["html_url"]
+            state = currentPull["state"]
 
-            pullRequests.append((repo_name, pull_id, title, login, creation_date, html_url));
+            pullRequests.append((repo_name, pull_id, title, login, creation_date, html_url, state));
         
         return pullRequests
     else:
@@ -74,7 +43,7 @@ def post_to_slack(pull_requests, slack_webhook):
     Posts the list of open pull requests provided to the slack thread 
     """
     for current_request in pull_requests:
-        slack_data = {'text': 'Lambda User: ' + current_request[3] + "\n" + current_request[5] + "\n\n"}
+        slack_data = {'text': 'Lambda User: ' + current_request[3] + "\n" + current_request[2] + ": " + current_request[6] + "\n" + current_request[5] + "\n\n"}
         response = requests.post(slack_webhook, data=json.dumps(slack_data), headers={'Content-Type': 'application/json'})
     
         if response.status_code != 200:
@@ -82,24 +51,21 @@ def post_to_slack(pull_requests, slack_webhook):
     
 
 def lambda_handler(event, context):
-    api_user='YOUR API user'
+    api_user='YOUR API User'
     api_token='YOUR Token'
     git_api_url='https://api.github.com'
     org_name=event['org']
-    #repo_name="quickstart-oracle-database"
+    repo_name=event['repo']
     slack_webhook='https://hooks.slack.com/services/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'
+    
 
-    repoList = repos_list(git_api_url, org_name, api_user, api_token)
-    #print(repoList)
-
-    for repo in repoList:
-        pullRequests = pulls_list(git_api_url, org_name, repo, api_user, api_token)
-        if len(pullRequests) > 0:
-            post_to_slack(pullRequests, slack_webhook)
+    pullRequests = pulls_list(git_api_url, org_name, repo_name, api_user, api_token)
+    if len(pullRequests) > 0:
+        post_to_slack(pullRequests, slack_webhook)
 
 
 if __name__ == '__main__':
-    event = {'org': 'aws-quickstart'}
+    event = {'org': 'ajavanshir', 'repo': 'git-to-slack'}
     lambda_handler(event, 'context')
    
             
